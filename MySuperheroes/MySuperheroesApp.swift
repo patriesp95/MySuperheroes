1e482d58fd4a1402a6206495f796494294722a9a
Patricia Martínez Espert <patrimespert@outlook.com>
2025-11-04 10:09:49 +0100

    Initial Commit

---
+++ MySuperheroes/MySuperheroesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySuperheroesApp.swift
+//  MySuperheroes
+//
+//  Created by Patricia M Espert on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MySuperheroesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
