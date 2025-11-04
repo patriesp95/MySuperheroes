97c624c38f4e8b0f4fdea20d3598eed31766512c
Patricia Martínez Espert <patrimespert@outlook.com>
2025-11-04 17:25:56 +0100

    Generates SuperheroDetail View and SuperheroDetailRow component

--- MySuperheroes/Detail/SuperheroDetail.swift
+++ MySuperheroes/Detail/SuperheroDetail.swift
@@ -9,13 +9,40 @@ import SwiftUI
 
 struct SuperheroDetail: View {
     let superHero: SuperHero
+    var powers: [String] {
+        superHero.poderes.map { String(describing: $0) }
+    }
     var body: some View {
         Form {
             Section {
-                Text(superHero.apodo)
-                Text(superHero.descripcion)
+                Image(superHero.imagen)
+                    .contextMenu {
+                        Button {
+                            
+                        } label: {
+                            Label("Compartir", systemImage: "square.and.arrow.up")
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Label("Email", systemImage: "paperplane")
+                        }
+                    }
+                
+            }
+            
+            Section {
+                LabeledContent("Nombre real", value: superHero.nombreReal)
+                LabeledContent("Edad", value: "\(superHero.edad) años")
+                LabeledContent("Poderes", value: "\(powers.formatted(.list(type: .and)))")
+                LabeledContent("Descripcion") {
+                    SuperheroDetailRow(text: superHero.descripcion, imageName: nil)
+                }
             } header: {
-                Text(superHero.nombreReal)
+                Text(superHero.apodo)
+                    .font(.title3)
+                    .foregroundStyle(.primary)
             }
 
         }
