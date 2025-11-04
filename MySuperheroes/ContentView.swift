cec625df4630686688548d0c4734c06ca8c51795
Patricia Martínez Espert <patrimespert@outlook.com>
2025-11-04 17:26:28 +0100

    Creates SuperheroesList and moves code from ContentView into it

--- MySuperheroes/ContentView.swift
+++ MySuperheroes/ContentView.swift
@@ -9,31 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationStack {
-            List {
-                ForEach(SUPERHEROES){ superHero in
-                    NavigationLink(value: superHero) {
-                        SuperheroesRow(superHero: superHero)
-                    }
-                }
-                .onDelete { indexSet in
-                    SUPERHEROES.remove(atOffsets: indexSet)
-                }
-                .onMove { indexSet, row in
-                    SUPERHEROES.move(fromOffsets: indexSet, toOffset: row)
-                }
-            }
-            .toolbar {
-                ToolbarItem {
-                    EditButton()
-                }
-            }
-            .navigationDestination(for: SuperHero.self) { superHero in
-                SuperheroDetail(superHero: superHero)
-            }
-            .navigationTitle("Superheroes")
-        }
-     
+        SuperheroesList()
     }
 }
 
